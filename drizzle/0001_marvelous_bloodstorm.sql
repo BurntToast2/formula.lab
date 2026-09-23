54eff416dc145c3fac31da2caa1274a47c1469d9
BurntToast2 <vissrawa79@gmail.com>
2026-09-23 21:48:41 +0100

    Database updated

---
+++ drizzle/0001_marvelous_bloodstorm.sql
@@ -0,0 +1,9 @@
+ALTER TYPE "public"."user_role" ADD VALUE 'admin';--> statement-breakpoint
+ALTER TYPE "public"."user_role" ADD VALUE 'Team Principal';--> statement-breakpoint
+ALTER TYPE "public"."user_role" ADD VALUE 'Technical Director';--> statement-breakpoint
+ALTER TYPE "public"."user_role" ADD VALUE 'Operations Manager';--> statement-breakpoint
+ALTER TYPE "public"."user_role" ADD VALUE 'Head of Chassis';--> statement-breakpoint
+ALTER TYPE "public"."user_role" ADD VALUE 'Head of Electronics';--> statement-breakpoint
+ALTER TYPE "public"."user_role" ADD VALUE 'Head of Suspension';--> statement-breakpoint
+ALTER TYPE "public"."user_role" ADD VALUE 'Head of Brakes';--> statement-breakpoint
+ALTER TYPE "public"."user_role" ADD VALUE 'Head of Drivetrain';
